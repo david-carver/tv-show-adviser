e070515a1297d237745d61ba26816ebe528fbcd4
David Carver <david.carver@verve.com>
2023-09-09 18:25:10 +0400

    update env var

--- src/config.js
+++ src/config.js
@@ -1,5 +1,5 @@
 const BASE_URL = "https://api.themoviedb.org/3/";
-const API_KEY_PARAM = "?api_key=6a6b012b4e778d237f148d7c0367a6b0";
+const API_KEY_PARAM = `?api_key=${process.env.REACT_APP_API_KEY}`;
 const BACKDROP_BASE_URL = "https://image.tmdb.org/t/p/original";
 const SMALL_IMG_BASE_URL = "https://image.tmdb.org/t/p/w300";
 
